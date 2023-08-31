ed4b8dcdfc3a301288c1961dd79783c56cd79be7
chqiu666 <108262121+chqiu666@users.noreply.github.com>
2023-08-31 18:06:36 +0800

    Create GPTcodetest01.swift
    gpt code with codepad plugin test 01

---
+++ GPTcodetest01.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var inputText: String = ""
+
+    var body: some View {
+        VStack {
+            TextField("Enter text", text: $inputText)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            HStack {
+                KeyButton(title: "Q", action: { appendCharacter("Q") })
+                KeyButton(title: "W", action: { appendCharacter("W") })
+                KeyButton(title: "E", action: { appendCharacter("E") })
+            }
+
+            HStack {
+                KeyButton(title: "A", action: { appendCharacter("A") })
+                KeyButton(title: "S", action: { appendCharacter("S") })
+                KeyButton(title: "D", action: { appendCharacter("D") })
+            }
+
+            HStack {
+                KeyButton(title: "Z", action: { appendCharacter("Z") })
+                KeyButton(title: "X", action: { appendCharacter("X") })
+                KeyButton(title: "C", action: { appendCharacter("C") })
+            }
+        }
+    }
+
+    func appendCharacter(_ char: String) {
+        inputText += char
+    }
+}
+
+struct KeyButton: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .frame(width: 50, height: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+    }
+}
+
+@main
+struct SoftKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
